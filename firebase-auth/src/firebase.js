52e6a7536b104dbab8aa5bb696cebef2c88b4f13
Daniel Giathi <blacksharkchi@proton.me>
2025-01-07 12:34:33 +0300

    create register and login methods

--- firebase-auth/src/firebase.js
+++ firebase-auth/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyCk63apN3cAZgP0ouKyEahMnYM5paoq1bs",
+  authDomain: "fir-auth-f1bea.firebaseapp.com",
+  projectId: "fir-auth-f1bea",
+  storageBucket: "fir-auth-f1bea.firebasestorage.app",
+  messagingSenderId: "929382482017",
+  appId: "1:929382482017:web:ac5dc972b990013bd66a20",
   measurementId: "G-8C8X7SJL40"
 };
 
--- firebase-auth/src/pages/Home.jsx
+++ firebase-auth/src/pages/Home.jsx
@@ -1,10 +1,47 @@
 import React, { useState } from 'react'
 
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+
+import { auth } from "../firebase"
+
 const Home = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const [isSignUpActive, setIsSignUpActive] = useState(false)
   const handleMethodChange = () => {
     setIsSignUpActive(!isSignUpActive)
   }
+
+  const handleSignUp = () => {
+    if (!email || !password) return;
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message
+      console.log(errorCode, errorMessage)
+    })
+  }
+
+  const handleSignIn = () => {
+    if (!email || !password) return;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message
+      console.log(errorCode, errorMessage)
+    })
+  }
+
+  const handleEmailChange = (event) => setEmail(event.target.value)
+  const handlePasswordChange = (event) => setPassword(event.target.value)
 
   return (
     <form>
@@ -16,15 +53,15 @@ const Home = () => {
         <ul>
           <li>
             <label htmlFor="email">Email</label>
-            <input type="email" id='email' />
+            <input type="email" id='email' onChange={handleEmailChange} />
           </li>
           <li>
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' />
+            <input type="password" id='password' onChange={handlePasswordChange} />
           </li>
         </ul>
-        {isSignUpActive && <button type='button'>Sign Up</button>}
-        {!isSignUpActive && <button type='button'>Sign In</button>}
+        {isSignUpActive && <button type='button' onClick={handleSignUp}>Sign Up</button>}
+        {!isSignUpActive && <button type='button' onClick={handleSignIn}>Sign In</button>}
         
       </fieldset>
       {isSignUpActive && <a onClick={handleMethodChange}>Login</a>}
