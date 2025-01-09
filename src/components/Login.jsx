11276a00e5b04eef84e45e643595508d04090cea
Daniel Giathi <blacksharkchi@proton.me>
2025-01-09 17:51:51 +0300

    add signin method with email and password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,29 @@
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
 import React, { useState } from 'react'
+import { toast } from "react-toastify"
 
 export const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+            toast.success("User registerred successfully", {
+                position: "top-right"
+            })
+        } catch (error) {
+            toast.success(error.message, {
+                position: "bottom-right"
+            })
+        }
+    }
+
   return (
-    <form >
+    <form onSubmit={handleLogin}>
         <h3>Login</h3>
         <div className='mb-3'>
             <label>Email</label>
