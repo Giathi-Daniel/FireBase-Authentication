5fab3467908417d4f62d073f3a12ac9a52e8c031
Daniel Giathi <blacksharkchi@proton.me>
2025-01-09 17:34:22 +0300

    refactor register and login forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
   return (
-    <div>Login</div>
+    <form >
+        <h3>Login</h3>
+        <div className='mb-3'>
+            <label>Email</label>
+            <input 
+                type="email" 
+                className="form-control" 
+                placeholder='Enter Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+        </div>
+
+        <div className='mb-3'>
+            <label>Password</label>
+            <input 
+                type="password" 
+                className="form-control" 
+                placeholder='Enter Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+        </div>
+
+        <div>
+            <button type='submit' className='btn btn-primary'>
+                Submit
+            </button>
+        </div>
+    </form>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Register = () => {
+export const Register = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [fname, setFname] = useState('')
+    const [lname, setLname] = useState('')
+
   return (
-    <div>Register</div>
+    <form >
+        <h3>Register</h3>
+        <div className='mb-3'>
+            <label>First Name</label>
+            <input 
+                type="text" 
+                className="form-control" 
+                placeholder='First Name'
+                onChange={(e) => setFname(e.target.value)}
+            />
+        </div>
+        <div className='mb-3'>
+            <label>Last Name</label>
+            <input 
+                type="text" 
+                className="form-control" 
+                placeholder='Last Name'
+                onChange={(e) => setLname(e.target.value)}
+            />
+        </div>
+        <div className='mb-3'>
+            <label>Email</label>
+            <input 
+                type="email" 
+                className="form-control" 
+                placeholder='Enter Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+        </div>
+
+        <div className='mb-3'>
+            <label>Password</label>
+            <input 
+                type="password" 
+                className="form-control" 
+                placeholder='Enter Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+        </div>
+
+        <div>
+            <button type='submit' className='btn btn-primary'>
+                Sign Up
+            </button>
+        </div>
+    </form>
   )
 }
-
-export default Register
